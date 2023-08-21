bd73a0874d0d6ac79087fb0747e2a57a049da030
Ahmet Tanrikulu a00835962 <ahmet.tanrikulu@huawei.com>
2023-08-21 19:15:19 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by admin on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

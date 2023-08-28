54f06afcc8fce0209d477839bfd7669ed72f9dd8
Ahmet Tanrikulu a00835962 <ahmet.tanrikulu@huawei.com>
2023-08-28 15:54:42 +0300

    init

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            
+            //MARK: - HEADER
+            
+            Spacer()
+            
+            
+            
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y: isAnimating ? 20 : -20)
+                    .animation(
+                        Animation
+                            .easeInOut(duration: 3)
+                            .repeatForever()
+                        , value: isAnimating
+                    )
+            }//: ZSTACK
+            //MARK: - CENTER
+            
+                Text("The time that leads to mastery is dependent on the intensity of our focus")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            //MARK: - FOOTER
+            
+            
+            
+            Spacer()
+            
+            
+            Button(action: {
+                withAnimation{
+                    isOnboardingViewActive = true
+                }
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3,design: .rounded))
+                    .fontWeight(.bold)
+            }// BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+        } //: VSTACK
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,  execute: {
+                isAnimating = true
+            })
+        })
     }
 }
 
